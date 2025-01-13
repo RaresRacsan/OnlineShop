ba1e1241fda3b8473c2cf9dc321aa4826435bb1b
916-Racsan-Rares <82086767+RaresRacsan@users.noreply.github.com>
2025-01-14 00:16:13 +0200

    #8 feature: added trending items on the main page

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../models/category');
+const db = require('../database/db');
 
-// Index route
 router.get('/', (req, res) => {
-    Category.getAllCategories((err, categories) => {
+    const getTrendingItems = `
+        SELECT p.*, c.name as category_name,
+        CAST(rating_sum AS FLOAT) / CASE WHEN rating_count = 0 THEN 1 ELSE rating_count END as avg_rating
+        FROM products p
+        JOIN categories c ON p.category_id = c.id
+        WHERE rating_count > 0
+        ORDER BY avg_rating DESC
+        LIMIT 20`;
+
+    db.all(getTrendingItems, [], (err, trendingItems) => {
         if(err) {
-            return res.status(500).send('Database error on getAllCategories');
+            return res.status(500).send('Database error');
         }
-        res.render('index', {categories});
+        Category.getAllCategories((err, categories) => {
+            if(err) {
+                return res.status(500).send('Database error');
+            }
+            res.render('index', { categories, trendingItems });
+        });
     });
 });
 
